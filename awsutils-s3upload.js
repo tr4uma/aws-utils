ea55143b936df8f0e738a43ee9dd211b018248c2
Marco Dagrada <dagrada.marco@hotmail.it>
2018-06-06 16:01:52 +0200

    [s3-uploader] configuration refactoring

--- awsutils-s3upload.js
+++ awsutils-s3upload.js
@@ -10,16 +10,17 @@ program
   .option('-s, --sourceDir <sourceDir>', 'Specify the directory containing all resources')
   .option('-t, --targetDir <targetDir>', 'Specify the target directory where data will be uploaded')
   .option('-c, --awsConfig <awsConfig>', 'AWS configuration file')
+  .option('-s3, --s3Config <s3Config>', 'S3 configuration file')
   .option('-b, --bucketName <bucketName>', 'S3 Bucket name')
   .parse(process.argv)
 
 
-const awsConfig = program.config === undefined ? __dirname+'/configs/aws/config.json' : program.config
-AWS.config.loadFromPath(__dirname+'/configs/aws/config.json')
+const awsConfig = program.awsConfig ? program.awsConfig : __dirname+'/configs/aws/config.json'
+AWS.config.loadFromPath(awsConfig)
 const s3 = new AWS.S3()
 
-const s3ConfigFile = program.awsConfig === undefined ? __dirname+'/configs/aws-config.json' : program.awsConfig
-const s3Config = JSON.parse(fs.readFileSync(__dirname+'/configs/aws-config.json', 'utf8'))
+const s3ConfigFile = program.s3Config ? program.s3Config : __dirname+'/configs/aws-config.json'
+const s3Config = JSON.parse(fs.readFileSync(s3ConfigFile, 'utf8'))
 
 const sourceDir = program.sourceDir ? program.sourceDir : './'
 const targetDir = program.targetDir ? program.targetDir : s3Config.targetFolder
